d79c6dfd486d29bc318729f45cd426d6d553fcd0
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2020-05-22 17:04:23 -0400

    Increase allowed json limit

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import compression from 'compression';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '60mb' }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(compression());
