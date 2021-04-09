b20e2c9ebad15d6e9f969157f6d45ade031d620c
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2021-04-09 08:23:45 -0400

    V1: Fix http response on failure

--- src/v1/exception.filter.ts
+++ src/v1/exception.filter.ts
@@ -7,9 +7,10 @@ export class AllExceptionsFilter implements ExceptionFilter {
     const response = ctx.getResponse();
     const request = ctx.getRequest();
     const status = (error instanceof HttpException) ? error.getStatus(): HttpStatus.INTERNAL_SERVER_ERROR;
+    const err = (error instanceof HttpException) ? error.getResponse(): undefined;
 
     response.status(status).json({
-      errors: [error.message],
+      errors: [err || error.message],
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
