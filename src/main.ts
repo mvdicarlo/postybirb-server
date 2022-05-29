b220ccba164245763b9688ebb0d6dab55f4ed220
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2022-05-29 14:33:44 -0400

    Add rate limiter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import compression from 'compression';
 import * as bodyParser from 'body-parser';
+import { rateLimit } from 'express-rate-limit';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,14 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(compression());
+
+  const limiter = rateLimit({
+    max: 25,
+    windowMs: 60_000 * 60
+  });
+
+  app.use(limiter);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
