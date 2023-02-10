c6736ba4534fbae994beaee54c44d73fcafcd3c0
Lemonynade <michael.v.dicarlo@gmail.com>
2023-02-10 12:12:39 -0500

    Update twitter-sensitive-media-warnings.enum.ts

--- src/v2/twitter/enums/twitter-sensitive-media-warnings.enum.ts
+++ src/v2/twitter/enums/twitter-sensitive-media-warnings.enum.ts
@@ -13,27 +13,6 @@ export enum ESensitiveMediaWarnings {
 export type ContentBlurType = 'other' | 'graphic_violence' | 'adult_content' | undefined;
 
 export namespace ESensitiveMediaWarnings_Utils {
-  /**
-   * Get matching ESensitiveMediaWarnings of given ESubmissionRating
-   * @param rating PB rating to convert to Twitter's warning tag
-   * @returns Matching ESensitiveMediaWarnings (Twitter warning tags) or
-   * undefined if none
-   */
-  export function getSMWFromRating(
-    rating: ESubmissionRating,
-  ): ESensitiveMediaWarnings {
-    switch (rating) {
-      case ESubmissionRating.MATURE:
-        return ESensitiveMediaWarnings.OTHER;
-      case ESubmissionRating.ADULT:
-        return ESensitiveMediaWarnings.ADULT_CONTENT;
-      case ESubmissionRating.EXTREME:
-        return ESensitiveMediaWarnings.GRAPHIC_VIOLENCE;
-      default:
-        return;
-    }
-  }
-
   export function getSMWFromContentBlur(
     contentBlur?: ContentBlurType,
   ) {
@@ -45,20 +24,7 @@ export namespace ESensitiveMediaWarnings_Utils {
       case 'graphic_violence':
         return ESensitiveMediaWarnings.GRAPHIC_VIOLENCE;
       default:
-        return;
+        return undefined;
     }
-  }
-
-  /**
-   * Get matching ESensitiveMediaWarnings of given string (ContentBlurType)
-   * @param contentBlurValue Value to convert
-   * @returns Matching ESensitiveMediaWarnings (Twitter warning tags) or
-   * undefined if none
-   * @throws RangeError when strVal is an invalid rating key
-   */
-  export function fromPBRatingStringValue(
-    contentBlurValue: ContentBlurType,
-  ): ESensitiveMediaWarnings {
-    return getSMWFromContentBlur(contentBlurValue);
   }
 }
